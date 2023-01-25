79d80a23034614ac458d07594909a9cac8bd128b
Gavin Monahan <gmonahan@kaya1.hpc.uwa.edu.au>
2023-01-25 13:27:40 +0800

    new fastq to bam wf

---
+++ launch_gatk_fastq_to_bam_wgs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH -c 36
+#SBATCH --job-name=fastq_to_bam_test
+#SBATCH --partition=day
+#SBATCH --account=hpi001
+#SBATCH --mem=256gb
+#SBATCH --time=1-00:00:00
+#SBATCH --export=NONE
+#SBATCH --mail-user=gavin.monahan@perkins.org.au
+#SBATCH --mail-type=END
+#SBATCH --error=%j.%x.err
+#SBATCH --output=%j.%x.out
+
+
+module load java/11.0.12
+source activate /home/gmonahan/.conda/envs/gatk4_pipeline/
+which python
+
+java -Dconfig.file=/group/hpi001/gatk/slurmCromwellGATK4/slurm_nochildren.conf -jar /group/hpi001/gatk/cromwell-72.jar run /group/hpi001/gatk/slurmCromwellGATK4/Multisample_Fastq_to_Bam_GATK4.wdl \
+   -i /group/hpi001/gatk/slurmCromwellGATK4/Multisample_Fastq_to_Bam_GATK4_inputs_hg38.json \
+   -o /group/hpi001/gatk/slurmCromwellGATK4/cromwell.options
